fd3288983c2ea2ce0b2ea1932159db2f43051263
Klem26 <Kankina26@gmail.com>
2021-03-02 12:31:01 +0200

    удалила лишний код

--- js/task-6.js
+++ js/task-6.js
@@ -4,29 +4,15 @@ const inputRef = document.querySelector('#validation-input');
 const dataLengthAtrRef = inputRef.getAttribute('data-length');
 console.log(dataLengthAtrRef)
 
-inputRef.addEventListener('focus', onInputFocus);
-inputRef.addEventListener('blur', onInputBlur);
 inputRef.addEventListener('input', onInputChange);
-
-function onInputFocus() {
-    console.log('Focus')
-}
-
-function onInputBlur() {
-    console.log('Blur')
-}
-
 
 function onInputChange(event) {
     const currentValue = event.target
-    // console.log(currentValue)
-  
-    if (currentValue.value.length >= dataLengthAtrRef) {
+    if (currentValue.value.length === Number(dataLengthAtrRef)) {
         currentValue.classList.add('valid') 
-        currentValue.classList.remove('invalid')
+        currentValue.classList.remove('invalid') 
     } else {
-        
         currentValue.classList.add('invalid') 
-        currentValue.classList.remove('valid')
+        currentValue.classList.remove('valid') 
      }
 }
